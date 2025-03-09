7184bfec2e7d7d9933b4be956225fd93deacb61d
EmmanuelJCA <emmanuelcanate@gmail.com>
2025-03-09 18:52:06 -0400

    feat: mailer endpoints

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,11 @@
 interface ImportMetaEnv {
-  readonly INSTAGRAM_ACCESS_TOKEN: string;
-  readonly YOUTUBE_CHANNEL_ID: string;
+  readonly EMAIL_SERVICE: string;
+  readonly EMAIL_ACCOUNT: string;
+  readonly EMAIL_PASSWORD: string;
+  readonly EMAIL_RECEPTOR: string;
   readonly YOUTUBE_API_KEY: string;
+  readonly YOUTUBE_CHANNEL_ID: string;
+  readonly INSTAGRAM_ACCESS_TOKEN: string;
 }
 
 interface ImportMeta {
---
+++ src/pages/api/mails/contacts.ts
@@ -0,0 +1,50 @@
+import type { APIRoute } from 'astro';
+
+import { internalErrorParser } from '@/utils/internal-error-parser';
+import { transporter, EMAIL_ACCOUNT, emailTemplate } from '@/utils/mail';
+
+const EMAIL_RECEPTOR = import.meta.env.EMAIL_RECEPTOR;
+
+export const POST: APIRoute = async ({ request }) => {
+  const data = await request.formData();
+
+  const name = data.get('name');
+  const email = data.get('email');
+  const phone = data.get('phone');
+  const message = data.get('message');
+  const subject = data.get('subject');
+
+  if (!name || !email || !message || !subject || !phone) {
+    return new Response(
+      JSON.stringify({
+        message: 'Missing required fields',
+      }),
+      { status: 400 }
+    );
+  }
+
+  const mailOptions = {
+    from: EMAIL_ACCOUNT,
+    to: EMAIL_RECEPTOR,
+    subject: 'C&C WEBSITE CONTACT FORM: ' + email,
+    html: emailTemplate(name.toString(), email.toString(), phone.toString(), message.toString(), subject.toString()),
+  };
+
+  try {
+    transporter.sendMail(mailOptions, (error) => {
+      if (error) {
+        throw new Error(error.message);
+      }
+    });
+
+    return new Response(
+      JSON.stringify({
+        message: 'Success!',
+      }),
+      { status: 200 }
+    );
+  } catch (e) {
+    const error = internalErrorParser(e);
+    return new Response(JSON.stringify(error), { status: 500 });
+  }
+};
